f79254f730047bebb0618d37691c068d92a53802
BRUNO GOMES DE BARROS FILHO <17104840@IFAL.LOCAL>
2020-03-02 12:08:31 -0300

    Versão base

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'typeface-roboto';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 import './App.css';
 
-function App() {
+
+class App extends React.Component{
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Typography>body1</Typography>
+      <TextField id="standard-basic" className="texto"/>
+      
+      <p>Fonte</p>  
     </div>
+    
   );
 }
-
+}
 export default App;
