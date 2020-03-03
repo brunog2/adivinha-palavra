7ad09040d5c5aabc9cebef8d53e783a024340fe2
BRUNO GOMES DE BARROS FILHO <17104840@IFAL.LOCAL>
2020-03-03 15:52:17 -0300

    Mais alteracoes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,47 @@ import React from 'react';
 import 'typeface-roboto';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import './App.css';
 
+class App extends React.Component {
 
-class App extends React.Component{
-render() {
-  return (
-    <div className="container">
-      <Typography>body1</Typography>
-      <TextField id="standard-basic" className="texto"/>
-      
-      <p>Fonte</p>  
-    </div>
-    
-  );
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      palavra: [
+        "Árvore",
+        "Carro",
+        "Casa",
+        "Natal",
+        "Computador"
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Typography component="div">
+          <Box className="titulo" fontSize={32} m={1}>Adivinhe a palavra</Box>
+        </Typography>
+        
+        
+        <div className="container-caracteres">
+          {this.state.palavra.map(caracteres => (
+              <TextField id="standard-basic" className="caractere" />
+          ))}
+        </div>
+
+
+
+
+        <p className="palavra">
+          {this.state.palavra[Math.floor(Math.random() * this.state.palavra.length)]}
+        </p>
+      </div>
+
+    );
+  }
 }
 export default App;
